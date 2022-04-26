5b65459a0768948c5eeeccc4434bca8aaabd5211
Guilherme Nais <guinais@hotmail.com>
2022-04-26 17:49:33 -0300

    commit aula php

---
+++ PHP/aula php/Index.php
@@ -0,0 +1,14 @@
+<html>
+
+   <head>
+      <title> Meu primeiro dite em PHP! Woohooo!</title>
+   </head>
+
+   <body>
+      <?php
+         for ( $i = 0  ; $i < 10 ;  $i++ ) {
+            print("Linhas número " . $i . " <br/>");
+          }
+       ?>
+   </body>
+</hmtl>
